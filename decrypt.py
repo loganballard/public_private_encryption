65a7f6f4eab071b6cad454b74623fdc8cf230647
loganballard <33067676+loganballard@users.noreply.github.com>
2019-03-08 16:36:03 -0800

    I think everything is working according to stupid trivial example, now just have to do IO

--- decrypt.py
+++ decrypt.py
@@ -1,12 +1,14 @@
-"""
-The decryption algorithm works as follows: To decrypt a ciphertext (C1, C2) with her private key d, Alice computes:
-•	(C1d)-1C2 mod p = m 		(that is the inverse of C1d mod p times C2)
-The inverse for C1 exists because, in general, x has a multiplicative inverse in Zn if x is relatively prime to n. Since p is prime then Zp = {1, …, p-1} and all of these values (the set of possible messages and ciphertext) are relatively prime to p.
-The decryption algorithm produces the intended message, because
-(C1d)-1∙C2 (mod p) ≡ ((gk)d )-1∙e2k∙m ≡ (gd k)-1∙e2k∙m ≡ ((gd)k)-1∙e2k∙m ≡ (e2k)-1∙e2k ∙m ≡ 1∙m = m.
-This works because the definition of inverses gives that, (x)-1∙x mod p = 1 and so we get that (x)-1∙x∙m mod p = m as long as m < p.	
+# faking 32 bit uints because python is too smart for data
+# types
+def fake32BitInt(num):
+    return num % 4294967295
 
-Recommended method for decryption: There is an easier way to compute the decryption. Instead of using m = (C1d)-1∙C2 mod p for decryption, we can avoid the calculation of multiplicative inverse and use m = C1p-1- d C2 mod p. This comes from Fermat’s Little Theorem: If p is a prime and a < p then ap-1 ≡ 1(mod p). So now the decryption algorithm works as follows: To decrypt a ciphertext (C1, C2) with her private key d, Alice computes:
-•	C1p-1- d∙C2 mod p = m 		
-The calculation is now ((C1p-1-d mod p)(C2 mod p)) mod p = m. 	
-"""
+# decryption based on fermats little theorum
+# takes private key and ciphertext msg as input
+# private key is a 32-bit uint, ciphertext msg
+# a dict of form:
+#   {'c1': c1, 'c2': c2}
+def decrypt(priKey, ciphertext, prime):
+    left = pow(ciphertext['c1'], prime-1-priKey, prime)
+    right = ciphertext['c2'] % prime
+    return (left * right) % prime 
